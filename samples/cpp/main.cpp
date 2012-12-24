2565b2e14081181cae72192d57a14cd046bf5824
yohe <ysekiguchi.zzz@gmail.com>
2012-12-25 00:10:17 +0900

    update

--- samples/cpp/main.cpp
+++ samples/cpp/main.cpp
@@ -3,6 +3,7 @@
 #include <test_case.hpp>
 #include <test_suite.hpp>
 #include <test_text_outputter.hpp>
+#include <test_junit_outputter.hpp>
 #include <test_macros.hpp>
 
 #include <fstream>
@@ -16,9 +17,36 @@ public:
 
     virtual void setup() {}
     virtual void teardown() {}
+    
+    int value() const {
+        return 2;
+    }
 
     void test_plus() {
         IUNIT_MESSAGE( "Test Plus" );
+        IUNIT_EQ(value(), 2);
+        IUNIT_ASSERT_EQ(value(), 2);
+        IUNIT_WARN_EQ(value(), 2);
+        IUNIT_NE(value(), 3);
+        IUNIT_ASSERT_NE(value(), 3);
+        IUNIT_WARN_NE(value(), 3);
+        int* a = new int(1);
+        IUNIT_NULL(NULL);
+        IUNIT_ASSERT_NULL(NULL);
+        IUNIT_WARN_NULL(NULL);
+        IUNIT_NOT_NULL(a);
+        IUNIT_ASSERT_NOT_NULL(a);
+        IUNIT_WARN_NOT_NULL(a);
+        delete a;
+        
+        IUNIT_LE(2, 3)
+        IUNIT_LT(2, 1)
+        IUNIT_GE(2, 3)
+        IUNIT_GT(2, 3)
+        IUNIT_ASSERT_LE(2, 1)
+        IUNIT_ASSERT_LT(2, 1)
+        IUNIT_ASSERT_GE(2, 3)
+        IUNIT_ASSERT_GT(2, 3)
     }
     void test_minus() {
     }
@@ -46,14 +74,16 @@ int main(int argc, char const* argv[])
     CppTestResultCollector collector;
     CppTestSuite suite("TestSuite", &collector);
     suite.addTest(new SampleTest());
-    suite.run();
+    suite.start();
 
     //original test suite
-    SampleSuite sampleSuite("SampleSuite", &collector);
-    sampleSuite.run();
+    //SampleSuite sampleSuite("SampleSuite", &collector);
+    //sampleSuite.run();
 
     std::ofstream ofs("result.txt");
     TextOutputter outputter(ofs);
+    JUnitOutputter outputter2(std::cout);
+    collector.write(&outputter2);
     collector.write(&outputter);
     return (collector.isSuccessful() ? 0 : -1);
 }
