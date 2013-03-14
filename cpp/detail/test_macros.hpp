fe555b0ff1c0e95fca12679ba97362a795901f6b
yohe <ysekiguchi.zzz@gmail.com>
2013-03-15 07:56:57 +0900

    Improved implementation of macro.

--- cpp/detail/test_macros.hpp
+++ cpp/detail/test_macros.hpp
@@ -3,137 +3,45 @@
 
 #include "detail/test_equal_checker.hpp"
 
-#define IUNIT_DETAIL_TEST(expected, actual, file, line, CHECK, TYPE) \
-    IUNIT_DETAIL_##TYPE##_##CHECK(expected, actual, file, line)
-
-#define IUNIT_DETAIL_ERROR_CHECK_IMPL(expected, actual, file, line, OP) \
+#define IUNIT_DETAIL_CHECK_IMPL(expected, actual, file, line, OP, TYPE) \
     { \
         if( expected OP actual ) { \
             this->success(); \
         } else { \
             this->failed(); \
             std::stringstream ss; \
-            ss << "Error :            " << #expected << " " << #OP << " " << #actual << std::endl; \
+            ss << #TYPE << " :            " << #expected << " " << #OP << " " << #actual << std::endl; \
             ss << "        expected = " << expected << std::endl;\
             ss << "          actual = " << actual; \
-            throw ErrorException(ss.str(), file, line); \
-        } \
-    }
-#define IUNIT_DETAIL_ASSERT_CHECK_IMPL(expected, actual, file, line, OP) \
-    { \
-        if( expected OP actual ) { \
-            this->success(); \
-        } else { \
-            this->failed(); \
-            std::stringstream ss; \
-            ss << "Assert :            " << #expected << " " << #OP << " " << #actual << std::endl; \
-            ss << "         expected = " << expected << std::endl;\
-            ss << "           actual = " << actual; \
-            throw AssertException(ss.str(), file, line); \
+            throw TYPE ## Exception(ss.str(), file, line); \
         } \
     }
 
-// Check Equal
-#define IUNIT_DETAIL_ASSERT_EQ(expected, actual, file, line) \
-    IUNIT_DETAIL_ASSERT_CHECK_IMPL(expected, actual, file, line, == ) \
-
-// Check Not Equal
-#define IUNIT_DETAIL_ASSERT_NE(expected, actual, file, line) \
-    IUNIT_DETAIL_ASSERT_CHECK_IMPL(expected, actual, file, line, != ) \
-
-// Check Less Equal
-#define IUNIT_DETAIL_ASSERT_LE(expected, actual, file, line) \
-    IUNIT_DETAIL_ASSERT_CHECK_IMPL(expected, actual, file, line, >= ) \
-
-// Check Less Than
-#define IUNIT_DETAIL_ASSERT_LT(expected, actual, file, line) \
-    IUNIT_DETAIL_ASSERT_CHECK_IMPL(expected, actual, file, line, > ) \
-
-// Check Greater Equal
-#define IUNIT_DETAIL_ASSERT_GE(expected, actual, file, line) \
-    IUNIT_DETAIL_ASSERT_CHECK_IMPL(expected, actual, file, line, <= ) \
-
-// Check Greater Than 
-#define IUNIT_DETAIL_ASSERT_GT(expected, actual, file, line) \
-    IUNIT_DETAIL_ASSERT_CHECK_IMPL(expected, actual, file, line, < ) \
-
-// Check Equal NULL
-#define IUNIT_DETAIL_ASSERT_NULL(expected, actual, file, line) \
+#define IUNIT_DETAIL_CHECK_THROW_IMPL(expression, EXCEPTION, file, line, TYPE) \
     { \
-        if( iunit::detail::EqualChecker::equal((void*)expected, (void*)actual) == false ) { \
+        try { \
+            expression; \
             this->failed(); \
             std::stringstream ss; \
-            ss << "Assert :         " << #actual << " == NULL" << std::endl; \
-            ss << "         but was " << #actual << "  = " << actual << std::endl;\
-            throw ErrorException(ss.str(), file, line); \
-        } else { \
+            ss << #TYPE << " :            " << #expression << " did not throw " << #EXCEPTION << std::endl; \
+            ss << "        expected = " << #expression << " should throw " << #EXCEPTION << std::endl;\
+            throw TYPE ## Exception(ss.str(), file, line); \
+        } catch ( EXCEPTION& e ) { \
             this->success(); \
         } \
     }
 
-// Check Not Equal NULL
-#define IUNIT_DETAIL_ASSERT_NOT_NULL(expected, actual, file, line) \
+#define IUNIT_DETAIL_CHECK_NO_THROW_IMPL(expression, file, line, TYPE) \
     { \
-        if( iunit::detail::EqualChecker::equal((void*)expected, (void*)actual) != false ) { \
+        try { \
+            expression; \
+            this->success(); \
+        } catch ( ... ) { \
             this->failed(); \
             std::stringstream ss; \
-            ss << "Assert :         " << #actual << " != NULL" << std::endl; \
-            ss << "         but was " << #actual << "  = " << actual; \
-            throw ErrorException(ss.str(), file, line); \
-        } else { \
-            this->success(); \
-        } \
-    }
-
-// Check Equal
-#define IUNIT_DETAIL_ERROR_EQ(expected, actual, file, line) \
-    IUNIT_DETAIL_ERROR_CHECK_IMPL(expected, actual, file, line, == ) \
-
-// Check Not Equal
-#define IUNIT_DETAIL_ERROR_NE(expected, actual, file, line) \
-    IUNIT_DETAIL_ERROR_CHECK_IMPL(expected, actual, file, line, != ) \
-
-// Check Less Equal
-#define IUNIT_DETAIL_ERROR_LE(expected, actual, file, line) \
-    IUNIT_DETAIL_ERROR_CHECK_IMPL(expected, actual, file, line, >= ) \
-
-// Check Less Than
-#define IUNIT_DETAIL_ERROR_LT(expected, actual, file, line) \
-    IUNIT_DETAIL_ERROR_CHECK_IMPL(expected, actual, file, line, > ) \
-
-// Check Greater Equal
-#define IUNIT_DETAIL_ERROR_GE(expected, actual, file, line) \
-    IUNIT_DETAIL_ERROR_CHECK_IMPL(expected, actual, file, line, <= ) \
-
-// Check Greater Than 
-#define IUNIT_DETAIL_ERROR_GT(expected, actual, file, line) \
-    IUNIT_DETAIL_ERROR_CHECK_IMPL(expected, actual, file, line, < ) \
-
-// Check Equal NULL
-#define IUNIT_DETAIL_ERROR_NULL(expected, actual, file, line) \
-    { \
-        if( iunit::detail::EqualChecker::equal((void*)expected, (void*)actual) == false ) { \
-            this->failed(); \
-            std::stringstream ss; \
-            ss << "Error :         " << #actual << " == NULL" << std::endl; \
-            ss << "        but was " << #actual << "  = " << actual; \
-            throw ErrorException(ss.str(), file, line); \
-        } else { \
-            this->success(); \
-        } \
-    }
-
-// Check Not Equal NULL
-#define IUNIT_DETAIL_ERROR_NOT_NULL(expected, actual, file, line) \
-    { \
-        if( iunit::detail::EqualChecker::equal((void*)expected, (void*)actual) != false ) { \
-            this->failed(); \
-            std::stringstream ss; \
-            ss << "Error :         " << #actual << " != NULL" << std::endl; \
-            ss << "        but was " << #actual << "  = " << actual;\
-            throw ErrorException(ss.str(), file, line); \
-        } else { \
-            this->success(); \
+            ss << #TYPE << " :            " << #expression << " threw exception." << std::endl; \
+            ss << "        expected = " << #expression << " don't throw exception." << std::endl;\
+            throw TYPE ## Exception(ss.str(), file, line); \
         } \
     }
 
--- cpp/test_macros.hpp
+++ cpp/test_macros.hpp
@@ -13,84 +13,84 @@ using namespace iunit::detail;
     addTest(this, &className::methodName, #className, #methodName)
 
 #define IUNIT_MESSAGE( message )   \
-    std::cout << message << std::endl;
-    //this->getResult().getOstream() << message << std::endl;
+    std::cout << message << std::endl; \
+    currentResult()->addMessage(message);
     
 //------------------------------------------------------------------------
 // Normal Check
 #define IUNIT_EQ(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, EQ, ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, ==, Error)
 
 #define IUNIT_NE(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, NE,ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, !=, Error)
 
 #define IUNIT_LE(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, LE, ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, >=, Error)
 
 #define IUNIT_LT(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, LT,ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, > , Error)
 
 #define IUNIT_GE(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, GE, ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, <=, Error)
 
 #define IUNIT_GT(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, GT,ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, < , Error)
 
 #define IUNIT_NULL( actual ) \
-    IUNIT_DETAIL_TEST(NULL, actual, __FILE__, __LINE__, NULL, ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(NULL, actual, __FILE__, __LINE__, ==, Error)
 
 #define IUNIT_NOT_NULL( actual ) \
-    IUNIT_DETAIL_TEST(NULL, actual, __FILE__, __LINE__, NOT_NULL, ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(NULL, actual, __FILE__, __LINE__, !=, Error)
 
 #define IUNIT_TRUE( condition ) \
-    IUNIT_DETAIL_TEST_COND( condition, __FILE__, __LINE__, TRUE, ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(true, (condition), __FILE__, __LINE__, ==, Error)
 
 #define IUNIT_FALSE( condition ) \
-    IUNIT_DETAIL_TEST_COND( condition, __FILE__, __LINE__, FALSE, ERROR)
+    IUNIT_DETAIL_CHECK_IMPL(false, (condition), __FILE__, __LINE__, ==, Error)
 
-#define IUNIT_THROW( exception ) \
-    IUNIT_DETAIL_TEST_THROW( exception, __FILE__, __LINE__, THROW, ERROR)
+#define IUNIT_THROW( expression, exception ) \
+    IUNIT_DETAIL_CHECK_THROW_IMPL( expression, exception, __FILE__, __LINE__, Error)
 
-#define IUNIT_NO_THROW( exception ) \
-    IUNIT_DETAIL_TEST_THROW( exception, __FILE__, __LINE__, NO_THROW, ERROR)
+#define IUNIT_NO_THROW( expression ) \
+    IUNIT_DETAIL_CHECK_NO_THROW_IMPL( expression, __FILE__, __LINE__, Error)
 
 //------------------------------------------------------------------------
 // Assertionl Check
 #define IUNIT_ASSERT_EQ(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, EQ, ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, ==, Assert)
 
 #define IUNIT_ASSERT_NE(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, NE, ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, !=, Assert)
 
 #define IUNIT_ASSERT_LE(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, LE, ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, >=, Assert)
 
 #define IUNIT_ASSERT_LT(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, LT,ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, > , Assert)
 
 #define IUNIT_ASSERT_GE(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, GE, ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, <=, Assert)
 
 #define IUNIT_ASSERT_GT(expected, actual) \
-    IUNIT_DETAIL_TEST(expected, actual, __FILE__, __LINE__, GT,ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(expected, actual, __FILE__, __LINE__, < , Assert)
 
 #define IUNIT_ASSERT_NULL( actual ) \
-    IUNIT_DETAIL_TEST(NULL, actual, __FILE__, __LINE__, NULL, ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(NULL, actual, __FILE__, __LINE__, ==, Assert)
 
 #define IUNIT_ASSERT_NOT_NULL( actual ) \
-    IUNIT_DETAIL_TEST(NULL, actual, __FILE__, __LINE__, NOT_NULL, ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(NULL, actual, __FILE__, __LINE__, !=, Assert)
 
 #define IUNIT_ASSERT_TRUE( condition ) \
-    IUNIT_DETAIL_TEST_COND(condition, __FILE__, __LINE__, TRUE, ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(true, (condition), __FILE__, __LINE__, ==, Assert)
 
 #define IUNIT_ASSERT_FALSE( condition ) \
-    IUNIT_DETAIL_TEST_COND(condition, __FILE__, __LINE__, FALSE, ASSERT)
+    IUNIT_DETAIL_CHECK_IMPL(false, (condition), __FILE__, __LINE__, ==, Assert)
 
-#define IUNIT_ASSERT_THROW( exception ) \
-    IUNIT_DETAIL_TEST_THROW( exception, __FILE__, __LINE__, THROW, ASSERT)
+#define IUNIT_ASSERT_THROW( expression, exception ) \
+    IUNIT_DETAIL_CHECK_THROW_IMPL( expression, exception, __FILE__, __LINE__, Assert)
 
-#define IUNIT_ASSERT_NO_THROW( exception ) \
-    IUNIT_DETAIL_TEST_THROW( exception, __FILE__, __LINE__, NO_THROW, ASSERT)
+#define IUNIT_ASSERT_NO_THROW( expression ) \
+    IUNIT_DETAIL_CHECK_NO_THROW_IMPL( expression, __FILE__, __LINE__, Assert)
 
 #endif /* end of include guard */
 
